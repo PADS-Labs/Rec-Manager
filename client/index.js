90672b993bfb53ff8bfd92f64ae3eb9780af3bff
wasafune <wasafune@gmail.com>
2018-01-29 16:49:46 -0800

    (set up) boiler plate

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+console.log('hi domo!')
+
+ReactDOM.render(<App />, document.getElementById('root'));
