5e261fa4f4deaa3d13a238a61be6d5ac0451ffab
tipdub <mstephenlin@gmail.com>
2018-01-31 13:55:15 -0800

    building auth

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,92 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom'
 
-export default class App extends React.Component {
+
+// const fakeAuth = {
+//   isAuthenticated: false,
+//   //method: fake the authentication process
+//   authenticate(cb){
+//     this.isAuthenticated = true
+//     setTimeout(cb, 100) //fake async login process
+//   }, //method: fake signout process
+//     this.isAuthenticated = false //change to false
+//     setTimeout(cb, 100)
+// }
+
+//components to render once we get to those routes
+// const Protected = () => <h3>Protected</h3> //show this component when authenticated, otherwise 
+// const Public = () => <h3>Public</h3> //redirect to here
+
+// class Login extends React.Component (
+//   state = {
+//     redirectToReferrer: false
+//   }
+
+//   login = () => {
+//     fakeAuth.authenticate(()=>{
+//       this.setState(()=>({
+//         redirectToReferrer: true
+//       }))
+//     })
+//   }
+
+//   render() {
+//     const { redirectToReferrer } = this.state
+//     const { from } this.props.location.state || { from: { pathname: '/'} }
+//     if (redirectToReferrer===true){
+//       return(
+//         <Redirect to={from} />
+//       )
+//     }
+
+//     return(
+//       <div>
+//         <p>You must log in to view this page </p>
+//         <button onClick={this.login}>Log in</button>
+//       </div>
+//     )
+//   }
+// )
+
+// const PrivateRoute = ({ component: Component, ...rest }) => (
+//   <Route {...rest} render={() => (
+// when path matches, then render component normally, but if NOT authenticated, redirect to login page
+//     fakeAuth.isAuthenticated === true ? <Component {...props}/> : <Redirect to='login' />
+//   )}
+// )
+
+//change to class App extends Component
+//export default App
+//OR 
+//use export default class App extends React.Component {
+class App extends Component {
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
+    <div style={{textAlign: 'center'}}>
         <h1>Hello World</h1>
-      </div>);
+    </div>
+
+      // <Router>
+      //   <div>
+      //     <ul>
+      //       <li><Link to='/public'>Public Page</Link></li>
+      //       <li><Link to='/protected'>Protected Page</Link></li>
+      //     </ul>
+
+      //     <Route path='/public' component={Public} />
+      //     <Route path='/login' component={Login} />
+      //     <PrivateRoute path='/protected' component={Protected} />
+      //   </div>
+      // </Router>
+
+    );
   }
 }
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, IndexRoute, Route} from 'react-router-dom';
 import App from './components/App.jsx';
 
 console.log('hi domo!')
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
