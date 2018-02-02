16ecfad36e66d5d62967f9e3b713edb4fde0cf6d
Dale Nogiec <dnogiec@aol.com>
2018-02-01 17:12:06 -0800

    Fixed POST Route and added DELETE route

--- routes/players.js
+++ routes/players.js
@@ -23,7 +23,7 @@ router.get('/:id', (request, response, next) => {
 router.post('/', (request, response, next) =>{
     const { firstname, lastname, email, username, googleid, administrator } = request.body;
         pool.query(
-            'INSERT INTO player(firstname, lastname, email, username, password, administrator) VALUES($1, $2, $3, $4, $5, $6)',
+            'INSERT INTO player(firstname, lastname, email, username, googleid, administrator) VALUES($1, $2, $3, $4, $5, $6)',
             [firstname, lastname, email, username, googleid, administrator],
             (err, res) => {
                 if (err) return next(err);
@@ -58,5 +58,15 @@ router.put('/:id', (request, response, next) => {
    
 });
 
+router.delete('/:id', (request, response, next) => {
+    const { id } = request.params;
+
+    pool.query(
+        `DELETE FROM player WHERE id=($1)`, [id], (err, res) => {
+            if (err) return next(err);
+            response.redirect('/')
+        }
+    )
+});
 
 module.exports = router; 
